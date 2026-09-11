9cdd74dc440c0c0477ba98d965a885a73cd35e5e
nbaldwin09 <nicholasbaldwin9007@gmail.com>
2026-09-11 11:23:19 +0100

    Drop careers routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,17 @@ import { Routes, Route } from "react-router-dom";
 import { Route as About } from "./routes/about";
 import { Route as Contact } from "./routes/contact";
 import { Route as Home } from "./routes/index";
-import { Route as Careers } from "./routes/careers";
 import { Route as Research } from "./routes/research";
 import { Route as Layout } from "./routes/route";
 
 export default function App() {
   const Shell = Layout.component;
-  const Jobs = Careers.component;
   return (
     <Routes>
       <Route path="/" element={<Shell />}>
         <Route index element={<Home.component />} />
         <Route path="about" element={<About.component />} />
         <Route path="research" element={<Research.component />} />
-        <Route path="careers" element={<Jobs />} />
-        <Route path="careers/:roleId" element={<Jobs />} />
         <Route path="contact" element={<Contact.component />} />
       </Route>
     </Routes>
