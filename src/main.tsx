8b545a8978649d99c95e516879df423ec89a271e
nbaldwin09 <nicholasbaldwin9007@gmail.com>
2026-09-11 08:43:51 +0100

    Mount Vercel Analytics in the React root

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App";
 import "./app.css";
 
@@ -9,6 +10,7 @@ createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <div data-brand="neem">
         <App />
+        <Analytics />
       </div>
     </BrowserRouter>
   </React.StrictMode>,
