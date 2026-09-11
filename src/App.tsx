0aa28da8c1c01e68873a5251214d87cb1b314649
nbaldwin09 <nicholasbaldwin9007@gmail.com>
2026-09-11 10:27:10 +0100

    Route About Us

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import { Routes, Route } from "react-router-dom";
-import { Route as R1 } from "./routes/contact";
-import { Route as R3 } from "./routes/index";
-import { Route as R7 } from "./routes/careers";
-import { Route as R8 } from "./routes/research";
-import { Route as R9 } from "./routes/route";
+import { Route as About } from "./routes/about";
+import { Route as Contact } from "./routes/contact";
+import { Route as Home } from "./routes/index";
+import { Route as Careers } from "./routes/careers";
+import { Route as Research } from "./routes/research";
+import { Route as Layout } from "./routes/route";
 
 export default function App() {
-  const Layout = R9.component;
-  const Careers = R7.component;
+  const Shell = Layout.component;
+  const Jobs = Careers.component;
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="contact" element={<R1.component />} />
-        <Route path="careers" element={<Careers />} />
-        <Route path="careers/:roleId" element={<Careers />} />
-        <Route index element={<R3.component />} />
-        <Route path="research" element={<R8.component />} />
+      <Route path="/" element={<Shell />}>
+        <Route index element={<Home.component />} />
+        <Route path="about" element={<About.component />} />
+        <Route path="research" element={<Research.component />} />
+        <Route path="careers" element={<Jobs />} />
+        <Route path="careers/:roleId" element={<Jobs />} />
+        <Route path="contact" element={<Contact.component />} />
       </Route>
     </Routes>
   );
