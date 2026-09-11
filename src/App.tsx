6860fa324f1a9a0141f8f4a61427ba42e25895c5
nbaldwin09 <nicholasbaldwin9007@gmail.com>
2026-09-11 10:18:02 +0100

    Route careers page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { Route as R1 } from "./routes/contact";
 import { Route as R3 } from "./routes/index";
+import { Route as R7 } from "./routes/careers";
 import { Route as R8 } from "./routes/research";
 import { Route as R9 } from "./routes/route";
 
@@ -10,6 +11,7 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="contact" element={<R1.component />} />
+        <Route path="careers" element={<R7.component />} />
         <Route index element={<R3.component />} />
         <Route path="research" element={<R8.component />} />
       </Route>
