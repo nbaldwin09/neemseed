3c04cd80aad3d5f8d2f8239821443152d47cc140
nbaldwin09 <nicholasbaldwin9007@gmail.com>
2026-09-11 10:01:16 +0100

    Drop about, involved, and investors routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import { Route as R0 } from "./routes/about";
 import { Route as R1 } from "./routes/contact";
-import { Route as R2 } from "./routes/culture";
 import { Route as R3 } from "./routes/index";
-import { Route as R4 } from "./routes/investors";
-import { Route as R5 } from "./routes/involved";
-import { Route as R6 } from "./routes/lab";
 import { Route as R8 } from "./routes/research";
 import { Route as R9 } from "./routes/route";
 
@@ -14,13 +9,8 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="about" element={<R0.component />} />
         <Route path="contact" element={<R1.component />} />
-        <Route path="culture" element={<R2.component />} />
         <Route index element={<R3.component />} />
-        <Route path="investors" element={<R4.component />} />
-        <Route path="involved" element={<R5.component />} />
-        <Route path="lab" element={<R6.component />} />
         <Route path="research" element={<R8.component />} />
       </Route>
     </Routes>
